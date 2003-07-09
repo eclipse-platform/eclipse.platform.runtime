b49c653ea748f049f4f370ca810d3356baa3a309
cvs2svn <platform-releng-dev@eclipse.org>
2003-07-09 20:37:39 +0000

    This commit was manufactured by cvs2svn to create tag 'v20030707'.
    Sprout from master 2003-07-07 18:19:51 UTC John Arthorne <johna> 'Added notion of system jobs'
    Cherrypick from master 2003-07-09 20:37:38 UTC John Arthorne <johna> 'Code cleanup':
        bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/plugins/PluginClassLoader.java
    Delete:
        bundles/org.eclipse.core.boot/.classpath
        bundles/org.eclipse.core.boot/.cvsignore
        bundles/org.eclipse.core.boot/.options
        bundles/org.eclipse.core.boot/.project
        bundles/org.eclipse.core.boot/about.html
        bundles/org.eclipse.core.boot/boot.xml
        bundles/org.eclipse.core.boot/build.properties
        bundles/org.eclipse.core.boot/plugin.properties
        bundles/org.eclipse.core.boot/plugin.xml
        bundles/org.eclipse.core.boot/splash.bmp
        bundles/org.eclipse.core.boot/src/org/eclipse/core/boot/BootLoader.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/boot/IPlatformConfiguration.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/boot/IPlatformRunnable.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/boot/package.html
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/BundleStats.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/ClassStats.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/ClassloaderStats.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/DelegatingURLClassLoader.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/InternalBootLoader.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/PlatformClassLoader.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/PlatformConfiguration.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/PlatformURLBaseConnection.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/PlatformURLConnection.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/PlatformURLHandler.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/PlatformURLHandlerFactory.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/PlatformURLHandlerFactoryProxy.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/Policy.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/ResourceEnumeration.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/ResourceLoader.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/URLContentFilter.java
        bundles/org.eclipse.core.boot/src/org/eclipse/core/internal/boot/messages.properties

--- bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/plugins/PluginClassLoader.java
+++ bundles/org.eclipse.core.runtime/src/org/eclipse/core/internal/plugins/PluginClassLoader.java
@@ -27,11 +27,9 @@ import org.eclipse.core.runtime.*;
  * PlatformClassLoader (assumes to be initialized by this point).
  *
  */
-
 public final class PluginClassLoader extends DelegatingURLClassLoader {
 	private PluginDescriptor descriptor;
-	private boolean pluginActivationInProgress = false;
-	private boolean loadInProgress = false;
+	private volatile boolean pluginActivationInProgress = false;
 	public static boolean usePackagePrefixes = true;
 
 public PluginClassLoader(URL[] codePath, URLContentFilter[] codeFilters, URL[] resourcePath, URLContentFilter[] resourceFilters, ClassLoader parent, PluginDescriptor descriptor) {
