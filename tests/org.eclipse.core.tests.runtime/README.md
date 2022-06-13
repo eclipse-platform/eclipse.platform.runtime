cc6bef06872fde286158a69a86bf17ae7e6d5c03
Lars Vogel <Lars.Vogel@vogella.com>
2022-06-13 15:43:15 +0200

    Retire migrated content from repo
    The content of the runtime repository has moved to
    https://github.com/eclipse-platform/eclipse.platform

--- tests/org.eclipse.core.tests.runtime/README.md
+++
@@ -1,7 +0,0 @@
-org.eclipse.core.tests.runtime
-==============================
-
-Dependencies
-============
-
-This plug-ins has an dependency to org.eclipse.test.performance from the https://git.eclipse.org/r/#/admin/projects/platform/eclipse.platform.releng repo.
--- tests/org.eclipse.e4.core.tests/src/org/eclipse/e4/core/internal/tests/di/extensions/FilterServiceA.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.e4.core.internal.tests.di.extensions;
-
-import org.osgi.service.component.annotations.Component;
-
-@Component(property = { "filtervalue=Test", "service.ranking:Integer=1" })
-public class FilterServiceA implements TestService {
-
-}
